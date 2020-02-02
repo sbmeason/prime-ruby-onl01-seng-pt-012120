62883aea2345676087a598f5941ef692f210b3cd
Samantha Meason <sbmeason@gmail.com>
2020-02-02 19:32:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,7 +1,7 @@
 def prime?(integer)
     i = 2
     while i < integer
-        return false if integer <= 1 && integer < 1
+        return false if integer <= 1 || integer <= 1
         i += 1
     end
     true

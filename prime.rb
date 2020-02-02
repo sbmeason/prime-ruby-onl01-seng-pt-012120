914d7af033ec65aaabef71f28edff6f50a1a1681
Samantha Meason <sbmeason@gmail.com>
2020-02-02 19:38:10 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,11 @@
-def prime?(integer)
-    i = 2
-    while i < integer
-        return false if integer <= 1 || integer <= 1
-        i += 1
-    end
-    true
+def prime?(x)
+  count = 0
+  
+  for each in (2..x)
+    x % each == 0 ? count += 1 : nil
+  end
+  
+  return count == 1 ? true : false
 end
  
   
